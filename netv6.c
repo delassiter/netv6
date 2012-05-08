a0e2538b1b3e0091a170bb9c8718b4087f570eec
Dennis Lassiter <dennis@pulseit.de>
2012-05-08 23:46:40 +0200

    Created empty main c-file

---
+++ netv6.c
@@ -0,0 +1,20 @@
+#include "php_netv6.h"
+zend_module_entry netv6_module_entry = {
+#if ZEND_MODULE_API_NO >= 20010901
+  STANDARD_MODULE_HEADER,
+#endif
+  PHP_NETV6_EXTNAME,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+#if ZEND_MODULE_API_NO >= 20010901
+  PHP_NETV6_EXTVER,
+#endif
+  STANDARD_MODULE_PROPERTIES
+};
+#ifdef COMPILE_DL_NETV6
+ZEND_GET_MODULE(netv6)
+#endif
