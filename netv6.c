692e4ab09dd10dac6acf5d8a2ae8379ca1592682
Dennis Lassiter <dennis@pulseit.de>
2012-05-09 00:29:36 +0200

    clean up c-file

--- netv6.c
+++ netv6.c
@@ -1,4 +1,32 @@
 #include "php_netv6.h"
+
+PHP_METHOD(NetV6, getHostByName)
+{
+    
+}
+
+zend_function_entry php_netv6_methods[] = {
+    PHP_ME(NetV6, getHostByName, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    {NULL, NULL, NULL}
+};
+
+PHP_MINIT_FUNCTION(netv6)
+{
+    zend_class_entry ce;
+    INIT_CLASS_ENTRY(ce, "NetV6", php_netv6_methods);
+    zend_register_internal_clas(&ce TSRMLS_CC);
+
+    return SUCCESS;
+}
+
+PHP_MINFO_FUNCTION(netv6)
+{
+    php_info_print_table_start();
+    php_info_print_table_row(2, "NetV6 Module", "enabled");
+    php_info_print_table_row(2, "version", PHP_NETV6_EXTVER);
+    php_info_print_table_end();
+}
+
 zend_module_entry netv6_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
   STANDARD_MODULE_HEADER,
@@ -18,29 +46,3 @@ zend_module_entry netv6_module_entry = {
 #ifdef COMPILE_DL_NETV6
 ZEND_GET_MODULE(netv6)
 #endif
-
-PHP_METHOD(NetV6, getHostByName)
-{
-    
-}
-
-zend_function_entry php_netv6_methods[] = {
-    PHP_ME(NetV6, getHostByName, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC);
-};
-
-PHP_MINIT_FUNCTION(netv6)
-{
-    zend_class_entry ce;
-    INIT_CLASS_ENTRY(ce, "NetV6", php_netv6_methods);
-    zend_register_internal_clas(&ce TSRMLS_CC);
-
-    return SUCCESS;
-}
-
-PHP_MINFO_FUNCTION(netv6)
-{
-    php_info_print_table_start();
-    php_info_print_table_row(2, "NetV6 Module", "enabled");
-    php_info_print_table_row(2, "version", PHP_NETV6_EXTVER);
-    php_info_print_table_end();
-}
